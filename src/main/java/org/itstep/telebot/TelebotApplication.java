53fd34c97424af40c76a69ba522595b9da439be3
Maxim <maxshaptala@gmail.com>
2019-03-23 11:37:45 +0200

    Удалены лишний код

--- src/main/java/org/itstep/telebot/TelebotApplication.java
+++ src/main/java/org/itstep/telebot/TelebotApplication.java
@@ -1,35 +1,16 @@
 package org.itstep.telebot;
 
-import com.google.api.gax.core.FixedCredentialsProvider;
-import com.google.api.gax.paging.Page;
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.auth.oauth2.ServiceAccountCredentials;
-import com.google.cloud.dialogflow.v2.*;
-import com.google.cloud.storage.Bucket;
-import com.google.cloud.storage.Storage;
-import com.google.cloud.storage.StorageOptions;
-import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.UUID;
-
 @SpringBootApplication
 @Slf4j
 public class TelebotApplication {
-
-    static boolean firstLoad = true;
-
     public static void main(String[] args) {
-
         ApiContextInitializer.init();
-        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
-
         SpringApplication.run(TelebotApplication.class, args);
     }
 }
--- src/main/java/org/itstep/telebot/bot/ChatBot.java
+++ src/main/java/org/itstep/telebot/bot/ChatBot.java
@@ -2,23 +2,12 @@ package org.itstep.telebot.bot;
 
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
-import org.itstep.telebot.actions.Command;
-import org.itstep.telebot.actions.DialogFlowCommand;
-import org.itstep.telebot.actions.StartCommand;
-import org.itstep.telebot.ai.DialogFlow;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.methods.AnswerCallbackQuery;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-
-import javax.inject.Inject;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 @Component
 @Slf4j
@@ -30,57 +19,21 @@ public class ChatBot extends TelegramLongPollingBot {
     @Value("${telegram.bot.user_name}")
     private String botUsername;
 
-    private final Map<String, Command> commands;
-    private final DialogFlowCommand dialogFlowCommand;
-
-    private final ExecutorService executorService;
-
-    @Inject
-    public ChatBot(DialogFlowCommand dialogFlowCommand, StartCommand startCommand) {
-        this.commands = new HashMap<>();
-        this.commands.put(StartCommand.CMD, startCommand);
-        this.dialogFlowCommand = dialogFlowCommand;
-        this.executorService = Executors.newCachedThreadPool();
-    }
-
     @Override
     public void onUpdateReceived(Update update) {
-        log.info("onUpdateReceived: " + update);
+        log.info("Receive message: " + update.getMessage());
         if (update.hasMessage() && update.getMessage().hasText()) {
-            log.info("message: " + update.getMessage());
-
-            String text = update.getMessage().getText().trim();
-            executorService.submit(() -> {
-                Command command = commands.getOrDefault(text, dialogFlowCommand);
-                command.execute(text, update, this);
-            });
-        }
-        if (update.hasCallbackQuery()) {
-            val callbackQuery = update.getCallbackQuery();
-            String data = callbackQuery.getData().trim();
-            log.info("data: " + data);
-            AnswerCallbackQuery answer = new AnswerCallbackQuery();
-            answer.setCallbackQueryId(callbackQuery.getId());
-            answer.setText(data);
-            //answer.setShowAlert(true);
-            executorService.submit(() -> {
-                try {
-                    execute(answer);
-                } catch (TelegramApiException e) {
-                    e.printStackTrace();
-                }
-            });
+            val msg = update.getMessage();
+            SendMessage message = new SendMessage()
+                    .setChatId(msg.getChatId())
+                    .setText(msg.getText());
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
         }
     }
-
-    @Override
-    public void onClosing() {
-        super.onClosing();
-        if (executorService != null && !executorService.isShutdown()) {
-            executorService.shutdownNow();
-        }
-    }
-
     @Override
     public String getBotUsername() {
         return botUsername;
