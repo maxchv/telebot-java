45263df8d4bb68f2a620da905469ec2e8a820703
Maxim <maxshaptala@gmail.com>
2019-03-23 11:43:15 +0200

    Подготовлен для демонстрации

--- src/main/java/org/itstep/telebot/TelebotApplication.java
+++ src/main/java/org/itstep/telebot/TelebotApplication.java
@@ -1,16 +1,11 @@
 package org.itstep.telebot;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.telegram.telegrambots.ApiContextInitializer;
-import org.telegram.telegrambots.meta.TelegramBotsApi;
 
 @SpringBootApplication
-@Slf4j
 public class TelebotApplication {
     public static void main(String[] args) {
-        ApiContextInitializer.init();
         SpringApplication.run(TelebotApplication.class, args);
     }
 }
